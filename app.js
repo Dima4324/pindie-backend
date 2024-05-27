4c8b8fe38c6d537bbd951b289eade0a92524ebfe
Dmitry Dutov <ocean.of.blood@mail.ru>
2024-05-27 19:09:59 +0300

    refund cors

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-const cors = require('cors')
 const path = require('path');
 const cookieParser = require("cookie-parser");
 const apiRouter = require("./routes/apiRouter")
 const pagesRouter = require("./routes/pages");
 
 const connectToDatabase = require('./database/connect');
-// const cors = require("./middlewares/cors");
+const cors = require("./middlewares/cors");
 
 const app = express();
 const PORT = 3001;
@@ -15,15 +14,7 @@ const PORT = 3001;
 connectToDatabase();
 
 app.use(
-  cors({
-    origin: [
-      'https://practicum.yandex.ru',
-      'https://students-projects.ru',
-      'http://localhost:3000',
-      'http://localhost:3001',
-      "https://pindie-practicum.nomoredomainswork.ru"
-    ],
-  }),
+  cors,
   cookieParser(),
   bodyParser.json(),
   pagesRouter,
